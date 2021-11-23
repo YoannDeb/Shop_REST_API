61cefea6fc283455655703f1e1af4a1ad7443321
Yoann <yoann.deblois@gmail.com>
2021-11-23 06:15:32 +0100

    func: Article serializer + extra fields

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,17 +1,24 @@
 from rest_framework.serializers import ModelSerializer
 
-from shop.models import Category, Product
+from shop.models import Category, Product, Article
 
 
 class CategorySerializer(ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['id', 'date_created', 'date_updated', 'name', 'description']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'description', 'active']
 
 
 class ProductSerializer(ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'date_created', 'date_updated', 'name', 'category_id', 'description']
+        fields = ['id', 'date_created', 'date_updated', 'name', 'category_id', 'description', 'active']
+
+
+class ArticleSerializer(ModelSerializer):
+
+    class Meta:
+        model = Article
+        fields = ['id', 'date_created', 'date_updated', 'name', 'price', 'product_id', 'description', 'active']
