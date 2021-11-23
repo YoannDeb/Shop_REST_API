c6dcf7b7b458e66eb93297b2b4e9d16732cc74df
Yoann <yoann.deblois@gmail.com>
2021-11-23 03:46:14 +0100

    func: usage of ViewSets

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
-from shop.views import CategoryAPIView, ProductAPIView
+from shop.views import CategoryViewSet, ProductViewSet
+
+router = routers.SimpleRouter()
+router.register('category', CategoryViewSet, basename='category')
+router.register('product', ProductViewSet, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/category/', CategoryAPIView.as_view()),
-    path('api/product/', ProductAPIView.as_view())
+    path('api/', include(router.urls))
 ]
