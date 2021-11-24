6e6ab445818686cb72ab6bb3757cd614cff1feb1
Yoann <yoann.deblois@gmail.com>
2021-11-24 06:00:49 +0100

    func: list and details model view sets for category and products

--- shop/views.py
+++ shop/views.py
@@ -1,37 +1,44 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from shop.models import Category, Product, Article
-from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
+from shop.serializers import CategoryListSerializer, CategoryDetailSerializer, ProductListSerializer, ProductDetailSerializer, ArticleSerializer
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
 
-    serializer_class = CategorySerializer
+    serializer_class = CategoryListSerializer
+    detail_serializer_class = CategoryDetailSerializer
 
     def get_queryset(self):
         queryset = Category.objects.all()
-
         if self.request.GET.get('show_inactive') != 'true':
             queryset = queryset.filter(active=True)
-
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer_class
+        return super().get_serializer_class()
 
 
 class ProductViewSet(ReadOnlyModelViewSet):
 
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
+    detail_serializer_class = ProductDetailSerializer
 
     def get_queryset(self):
         queryset = Product.objects.all()
-
         if self.request.GET.get('show_inactive') != 'true':
             queryset = queryset.filter(active=True)
-
         category_id = self.request.GET.get('category_id')
         if category_id:
             queryset = queryset.filter(category_id=category_id)
-
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer_class
+        return super().get_serializer_class()
 
 
 class ArticleViewSet(ReadOnlyModelViewSet):
@@ -39,16 +46,12 @@ class ArticleViewSet(ReadOnlyModelViewSet):
     serializer_class = ArticleSerializer
 
     def get_queryset(self):
-
         queryset = Article.objects.all()
-
         if self.request.GET.get('show_inactive') != 'true':
             queryset = queryset.filter(active=True)
-
         product_id = self.request.GET.get('product_id')
         if product_id:
             queryset = queryset.filter(product_id=product_id)
-
         return queryset
 
 
