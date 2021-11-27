1905c5051d5ffaddaed6206f11ca9bc53fdf19f2
Yoann <yoann.deblois@gmail.com>
2021-11-27 07:33:04 +0100

    func: articles admin ModelView

--- project/urls.py
+++ project/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from shop.views import CategoryViewSet, ProductViewSet, ArticleViewSet, AdminCategoryViewSet
+from shop.views import CategoryViewSet, ProductViewSet, ArticleViewSet, AdminCategoryViewSet, AdminArticleViewSet
 
 router = routers.SimpleRouter()
 router.register('category', CategoryViewSet, basename='category')
 router.register('product', ProductViewSet, basename='product')
 router.register('article', ArticleViewSet, basename='article')
 router.register('admin/category', AdminCategoryViewSet, basename='admin-category')
+router.register('admin/article', AdminArticleViewSet, basename='admin-article')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
